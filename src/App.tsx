77b35504f49820f70daaecf1123caaa95b3fc0c9
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2025-02-14 21:54:14 -0300

    commit to test husky

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
