f7d4c30a21bf5941241bbe6d69b354e50fe670b7
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2025-02-14 22:05:41 -0300

    commit to test husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Component1 from './components/component1'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
 
         
       </p>
+      <Component1 />
     </>
   )
 }
